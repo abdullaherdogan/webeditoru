876862cb28caaffa0627fa0a8e4eeef706ddec16
abdullah <abdullah-erdgn@hotmail.com>
2020-06-19 18:59:39 +0300

    leetcode67

---
+++ leet67.js
@@ -0,0 +1,8 @@
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+  };
+
+console.log(addBinary(101,110))
